87e04ba672f41fa06c977314efe1fded61726fc1
adityapratap1905 <adityapratap1905@gmail.com>
2026-03-10 21:43:29 +0530

    Commit

--- frontend_flutter/lib/main.dart
+++ frontend_flutter/lib/main.dart
@@ -5,6 +5,8 @@ import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
 import 'screens/dashboard_screen.dart';
 import 'screens/profile_screen.dart';
+import 'screens/find_clinics_screen.dart';
+import 'screens/system_log_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,6 +31,8 @@ class MyApp extends StatelessWidget {
         '/signup': (context) => SignupScreen(),
         '/dashboard': (context) => DashboardScreen(),
         '/profile': (context) => const ProfileScreen(),
+        '/find-clinics': (context) => const FindClinicsScreen(),
+        '/system-log': (context) => const SystemLogScreen(),
       },
 
       theme: ThemeData(
